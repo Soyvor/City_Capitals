5dd77cc760e127ec546774efcaa87c3dab89e8f7
Yajushreshtha <97739937+Soyvor@users.noreply.github.com>
2023-04-08 22:22:58 +0530

    Update main.java

--- main.java
+++ main.java
@@ -6,34 +6,34 @@ public class StateCapitalFinder {
     public static void main(String[] args) {
         // Define a HashMap that maps each state to its corresponding capital city
         HashMap<String, String> stateToCapital = new HashMap<String, String>();
-        stateToCapital.put("Andhra Pradesh", "Hyderabad");
-        stateToCapital.put("Arunachal Pradesh", "Itanagar");
-        stateToCapital.put("Assam", "Dispur");
-        stateToCapital.put("Bihar", "Patna");
-        stateToCapital.put("Chhattisgarh", "Raipur");
-        stateToCapital.put("Goa", "Panaji");
-        stateToCapital.put("Gujarat", "Gandhinagar");
-        stateToCapital.put("Haryana", "Chandigarh");
-        stateToCapital.put("Himachal Pradesh", "Shimla");
-        stateToCapital.put("Jharkhand", "Ranchi");
-        stateToCapital.put("Karnataka", "Bengaluru");
-        stateToCapital.put("Kerala", "Thiruvananthapuram");
-        stateToCapital.put("Madhya Pradesh", "Bhopal");
-        stateToCapital.put("Maharashtra", "Mumbai");
-        stateToCapital.put("Manipur", "Imphal");
-        stateToCapital.put("Meghalaya", "Shillong");
-        stateToCapital.put("Mizoram", "Aizawl");
-        stateToCapital.put("Nagaland", "Kohima");
-        stateToCapital.put("Odisha", "Bhubaneswar");
-        stateToCapital.put("Punjab", "Chandigarh");
-        stateToCapital.put("Rajasthan", "Jaipur");
-        stateToCapital.put("Sikkim", "Gangtok");
-        stateToCapital.put("Tamil Nadu", "Chennai");
-        stateToCapital.put("Telangana", "Hyderabad");
-        stateToCapital.put("Tripura", "Agartala");
-        stateToCapital.put("Uttar Pradesh", "Lucknow");
-        stateToCapital.put("Uttarakhand", "Dehradun");
-        stateToCapital.put("West Bengal", "Kolkata");
+        stateToCapital.put("andhra Pradesh", "Hyderabad");
+        stateToCapital.put("arunachal Pradesh", "Itanagar");
+        stateToCapital.put("assam", "Dispur");
+        stateToCapital.put("bihar", "Patna");
+        stateToCapital.put("chhattisgarh", "Raipur");
+        stateToCapital.put("goa", "Panaji");
+        stateToCapital.put("gujarat", "Gandhinagar");
+        stateToCapital.put("haryana", "Chandigarh");
+        stateToCapital.put("himachal Pradesh", "Shimla");
+        stateToCapital.put("jharkhand", "Ranchi");
+        stateToCapital.put("karnataka", "Bengaluru");
+        stateToCapital.put("kerala", "Thiruvananthapuram");
+        stateToCapital.put("madhya Pradesh", "Bhopal");
+        stateToCapital.put("maharashtra", "Mumbai");
+        stateToCapital.put("manipur", "Imphal");
+        stateToCapital.put("meghalaya", "Shillong");
+        stateToCapital.put("mizoram", "Aizawl");
+        stateToCapital.put("nagaland", "Kohima");
+        stateToCapital.put("odisha", "Bhubaneswar");
+        stateToCapital.put("punjab", "Chandigarh");
+        stateToCapital.put("rajasthan", "Jaipur");
+        stateToCapital.put("sikkim", "Gangtok");
+        stateToCapital.put("tamil Nadu", "Chennai");
+        stateToCapital.put("telangana", "Hyderabad");
+        stateToCapital.put("tripura", "Agartala");
+        stateToCapital.put("uttar Pradesh", "Lucknow");
+        stateToCapital.put("uttarakhand", "Dehradun");
+        stateToCapital.put("west Bengal", "Kolkata");
 
         // Create a Scanner object to read user input
         Scanner scanner = new Scanner(System.in);
@@ -41,6 +41,7 @@ public class StateCapitalFinder {
         // Prompt the user to enter a state name
         System.out.print("Enter the name of a state in India: ");
         String stateName = scanner.nextLine();
+        stateName=stateName.toLowerCase();
 
         // Look up the corresponding capital city in the HashMap
         String capitalCity = stateToCapital.get(stateName);
